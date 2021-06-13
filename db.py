866ea9895f3b90956d01b54c5a0073505f006a77
andres kouvach <akouvach@yahoo.com>
2021-06-13 11:57:11 -0300

    consulta a base de datos

---
+++ db.py
@@ -0,0 +1,82 @@
+import sqlite3
+DBNAME = "indicadores.db"
+
+def openDb():
+    try:
+        sqliteConnection = sqlite3.connect(DBNAME)
+        print("Database connected successfully")
+
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+        raise Exception("error en la conexión de la base de datos")
+
+    return sqliteConnection
+
+def closeDb(conn):
+    try:
+        conn.close()
+        print("Database successfully closed")
+
+    except sqlite3.Error as error:
+        print("Error while closing", error)
+        raise Exception("error en el cierre de la base de datos")
+
+    return True
+
+
+
+def getDbVersion():
+    try:
+        dbConn = openDb()
+        cursor = dbConn.cursor()
+        sqlite_select_Query = "select sqlite_version();"
+        cursor.execute(sqlite_select_Query)
+        record = cursor.fetchall()
+        print("SQLite Database Version is: ", record)
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if dbConn:
+            closeDb(dbConn)
+            print("The SQLite connection is closed")
+
+
+
+def getVariables():
+    try:
+        dbConn = openDb()
+        cursor = dbConn.cursor()
+        sqlite_select_Query = "select * from variables;"
+        cursor.execute(sqlite_select_Query)
+
+
+        records = cursor.fetchall()
+        print("Total rows are:  ", len(records))
+        print("Printing each row", records[0])
+        for row in records:
+            print("Id: ", row[0])
+            print("descripcion: ", row[1])
+            print("formula: ", row[2])
+            print("agruparpor: ", row[3])
+            print("\n")
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Error while connecting to variables", error)
+    finally:
+        if dbConn:
+            closeDb(dbConn)
+            print("The SQLite connection is closed")
+
+
+
+
+
+
+
+
+
+
